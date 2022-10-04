d163e577a1c2f68f29e61f1380abe01e892a9523
Susan Brown <brown684408@gmail.com>
2022-10-04 15:57:44 -0500

    create and run about component test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+//This is the main file of the application. To effect any change on the application, we need to either modify this file or add components inside it.
+//need to import React in every component file
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import About from '..';
+
+afterEach(cleanup);
+describe('About component', () => {
+    //renders About test
+    // First Test - baseline testy to verify that the component is rendering
+    it('renders', () => {
+        render(<About />);
+    });
+
+    const { asFragment } = render(<About />);
+    expect(asFragment()).toMatchSnapshot();
+
+    // Second Test
+    it('matches snapshot DOM node structure', () => {
+        // render About
+    });
+})
+
+
+
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
